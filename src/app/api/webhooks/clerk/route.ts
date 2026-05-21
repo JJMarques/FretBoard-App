2f4711c4cc5706f620a4aef3187643b148633145
José Marques <jjgm98@gamil.com>
2026-05-21 22:25:51 +0100

    feat: added hero page for unauthenticated users

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -16,6 +16,7 @@ export async function POST(req: Request) {
     const svix_id = headerPayload.get('svix-id');
     const svix_timestamp = headerPayload.get('svix-timestamp');
     const svix_signature = headerPayload.get('svix-signature');
+    
     if (!svix_id || !svix_timestamp || !svix_signature) {
         return new Response('Invalid signature', { status: 400 });
     };
